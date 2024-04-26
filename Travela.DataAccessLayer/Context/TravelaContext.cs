4768f2a5b7319ee0ce8ed93f50462eb0052bef7d
MuratYucedag <yucedagmurat23@gmail.com>
2024-04-26 20:36:20 +0300

    Add project files.

---
+++ Travela.DataAccessLayer/Context/TravelaContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Travela.EntityLayer.Concrete;
+
+namespace Travela.DataAccessLayer.Context
+{
+    public class TravelaContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-R7AR1ND;initial Catalog=TravelaDb;integrated Security=true;");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Destination> Destinations { get; set; }
+    }
+}
---
+++ Travela.EntityLayer/Concrete/Destination.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travela.EntityLayer.Concrete
+{
+    public class Destination
+    {
+        public int DestinationId { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string ImageUrl { get; set; }
+        public int CountDay { get; set; }
+        public string SubTitle { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
