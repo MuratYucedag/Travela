23e36e5a9dfb11647c2ac19ae19fb4040359345f
MuratYucedag <yucedagmurat23@gmail.com>
2024-05-02 20:43:47 +0300

    ...

--- Travela.WebUI/Controllers/DestinationController.cs
+++ Travela.WebUI/Controllers/DestinationController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Text;
 using Travela.WebUI.Dtos;
 
 namespace Travela.WebUI.Controllers
@@ -17,11 +18,32 @@ namespace Travela.WebUI.Controllers
             var responseMessage = await client.GetAsync("https://localhost:7221/api/Destination");
             if (responseMessage.IsSuccessStatusCode)
             {
-                var jsonData=await responseMessage.Content.ReadAsStringAsync();
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<ResultDestinationDto>>(jsonData);
                 return View(values);
             }
             return View();
         }
+
+        [HttpGet]
+        public IActionResult CreateDestination()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateDestination(CreateDestinationDto createDestinationDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createDestinationDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7221/api/Destination", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("DestinationList");
+            }
+            return View();
+        }
+
     }
 }
